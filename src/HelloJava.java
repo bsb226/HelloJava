bf3e7181390c5ecec200cade3543f5ab0a28587c
Brian Borowski <brian_borowski@yahoo.com>
2015-03-03 23:14:41 -0500

    Added Javadoc comments.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,4 +1,8 @@
-
+/** 
+ * Basic "Hello, world!" Java class.
+ * @author Brian Borowski
+ * @version 1.0, last modified March 3, 2015
+ */
 public class HelloJava {
 
     public static void main(String[] args) {
