c5968bc882b1c251ea1d3e26641fa5211621bc63
Brian Borowski <brian_borowski@yahoo.com>
2015-03-24 11:28:25 -0400

    Added a new class with methods that return sayings.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,11 +1,11 @@
 /** 
  * Basic "Hello, world!" Java class.
  * @author Brian Borowski
- * @version 1.0, last modified March 3, 2015
+ * @version 1.1, last modified March 24, 2015
  */
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        System.out.println(Sayings.sayHi());
     }
 }
---
+++ src/Sayings.java
@@ -0,0 +1,11 @@
+/** 
+ * Class containing various sayings.
+ * @author Brian Borowski
+ * @version 1.0, last modified March 24, 2015
+ */
+public class Sayings {
+
+    public static String sayHi() {
+        return "Hi, Java!";
+    }
+}
