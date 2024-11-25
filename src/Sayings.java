ce010d628fec191117247b2f38777242f73c2652
Brian Borowski <brian_borowski@yahoo.com>
2024-11-25 10:39:49 -0500

    Add saying.

--- src/Sayings.java
+++ src/Sayings.java
@@ -1,11 +1,17 @@
 /** 
  * Class containing various sayings.
  * @author Brian Borowski
- * @version 1.0, last modified March 24, 2015
+ * @version 1.0.1 
+ * Created on March 24, 2015
+ * Last modified on November 25, 2024
  */
 public class Sayings {
 
     public static String sayHi() {
         return "Hi, Java!";
+    }
+
+    public static String sayBye() {
+        return "Bye, Java!";
     }
 }
