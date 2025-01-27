e145be408029a5fc17915a67e00a5dae397e2b0e
Brian Borowski <bsb2151@columbia.edu>
2025-01-27 13:24:53 -0500

    Update date.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,7 +1,7 @@
 /** 
- * Basic "Hello, world!" Java class.
+ * Basic "Hello, world!" Java class
  * @author Brian Borowski
- * @version 1.1, last modified March 24, 2015
+ * @version 1.2, last modified Jan. 27, 2025
  */
 public class HelloJava {
 
